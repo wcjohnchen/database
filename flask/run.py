f1028773685d8f95261c700aca1f6d6d066610ff
wcjohnchen <wcjohnchen@gmail.com>
2025-04-14 11:05:07 -0400

    commit

--- flask/run.py
+++ flask/run.py
@@ -9,7 +9,7 @@ import psycopg2.extras
 app = Flask(__name__)
 
 
-@app.route('/') # Homepage
+@app.route('/')
 def home():
     return render_template('index.html')
 
@@ -23,11 +23,11 @@ def query():
         genename = request.form.get('gene')
 
 
-        hostname = 'localhost' # <--- enter hostname
-        database = 'mydatabase' # <--- enter database name
-        username = 'wcjohnchen2022' # <--- enter username
-        pwd = 'guaiguai0730C' # <--- enter password
-        port_id = 5432 # <--- enter port id
+        hostname = '' # <--- enter hostname
+        database = '' # <--- enter database name
+        username = '' # <--- enter username
+        pwd = '' # <--- enter password
+        port_id =  # <--- enter port id
         conn = None
         tablename= 'polyadb4_lr' # <--- enter table name
 
